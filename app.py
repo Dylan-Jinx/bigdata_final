03a53fd32e4530751fcf4d0e51200eb0b633c465
Dylan Jinx <1012140020@qq.com>
2022-10-21 22:16:26 +0800

    add multilayer perception and heatmap kde map and so on fix bug

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
+import cmd
+import os
+import subprocess
+
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 
 import settings
 from common.JsonFlask import JsonFlask
+from logs_output_core.generation_log import red_logs
+
 # 注意导包顺序 因为别的包已经要用到db了 这时候db必须在那些包前面初始化
 app = JsonFlask(__name__)
 app.config.from_object(settings.Configs)
@@ -14,11 +20,15 @@ from controller.single_various_analysis_controller import singleVariousModule
 from controller.double_various_analysis_controller import doubleVariousModule
 from model import BigData
 from utils.O2d import O2d
+
 CORS(app, resources={r"/*": {"origins": "*"}})
 app.register_blueprint(cityDataModule, url_prefix='/cityData')
 app.register_blueprint(statisticsModule, url_prefix='/statistics')
 app.register_blueprint(singleVariousModule, url_prefix='/singleVarious')
 app.register_blueprint(doubleVariousModule, url_prefix='/doubleVarious')
+
+line_number = [0]
+x = None
 
 
 @app.route('/')
@@ -26,10 +36,36 @@ def hello_world():
     return ApiResponse.success(data=O2d.obj_to_list(BigData.query.all()))
 
 
+@app.route("/getLogs")
+def get_log():
+    log_data = red_logs()  # 获取日志
+    # 判断如果此次获取日志行数减去上一次获取日志行数大于0，代表获取到新的日志
+    if len(log_data) - line_number[0] > 0:
+        log_type = 2  # 当前获取到日志
+        log_difference = len(log_data) - line_number[0]  # 计算获取到少行新日志
+        log_list = []  # 存放获取到的新日志
+        # 遍历获取到的新日志存放到log_list中
+        for i in range(log_difference):
+            log_i = log_data[-(i + 1)].decode('utf-8')  # 遍历每一条日志并解码
+            log_list.insert(0, log_i)  # 将获取的日志存放log_list中
+    else:
+        log_type = 3
+        log_list = ''
+    # 已字典形式返回前端
+    _log = {
+        'log_type': log_type,
+        'log_list': log_list
+    }
+    line_number.pop()  # 删除上一次获取行数
+    line_number.append(len(log_data))  # 添加此次获取行数
+    return _log
+
+
 # @app.errorhandler(Exception)
 # def error_handler(e):
 #     return ApiResponse.error(msg=str(e))
-#
+
+
 
 if __name__ == '__main__':
     app.run()
--- utils/list_object_deal.py
+++ utils/list_object_deal.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 from model import BigData
 
 
@@ -32,3 +34,26 @@ def get_key_val_by_query_datas(datas):
         key.append(i[0])
         value.append(i[1])
     return key, value
+
+
+def all_data_convert_dataFrame(datas):
+    testxx = {
+        "城市可支配收入": get_list_selected_attr_by_attrname(datas, "city_able_income"),
+        "城市可支配收入与收入比": get_list_selected_attr_by_attrname(datas, "city_able_income_ratio"),
+        "城市总收入": get_list_selected_attr_by_attrname(datas, "city_income"),
+        "城市收入与城市人口比 ": get_list_selected_attr_by_attrname(datas, "city_income_population_ratio"),
+        "城市人口": get_list_selected_attr_by_attrname(datas, "city_population"),
+        "城市人口与总人口比": get_list_selected_attr_by_attrname(datas, "city_population_ratio"),
+        "城镇化": get_list_selected_attr_by_attrname(datas, "city_ratio"),
+        "总收入": get_list_selected_attr_by_attrname(datas, "income"),
+        "总人口": get_list_selected_attr_by_attrname(datas, "population"),
+        "泰尔指数": get_list_selected_attr_by_attrname(datas, "theil_index"),
+        "乡村可支配收入": get_list_selected_attr_by_attrname(datas, "village_able_income"),
+        "乡村可支配收入与收入比": get_list_selected_attr_by_attrname(datas, "village_able_income_ratio"),
+        "乡村收入": get_list_selected_attr_by_attrname(datas, "village_income"),
+        "乡村收入与人口比 ": get_list_selected_attr_by_attrname(datas, "village_income_population_ratio"),
+        "乡村人口": get_list_selected_attr_by_attrname(datas, "village_population"),
+        "乡村人口与重人口比": get_list_selected_attr_by_attrname(datas, "village_population_ratio")
+    }
+    df = pd.DataFrame(testxx)
+    return df
