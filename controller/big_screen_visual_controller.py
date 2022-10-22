eb26a84c17c6388825c596fac83532675e641e29
Dylan Jinx <1012140020@qq.com>
2022-10-22 14:40:43 +0800

    the final project_ firstly commit

--- controller/big_screen_visual_controller.py
+++ controller/big_screen_visual_controller.py
@@ -1,3 +1,6 @@
+import threading
+import time
+
 from flask import Blueprint, request
 from sqlalchemy import between, create_engine
 from sqlalchemy.orm import sessionmaker
@@ -13,14 +16,17 @@ bigScreenVisualModule = Blueprint('bigScreenVisualModule', __name__)
 
 @bigScreenVisualModule.route("/getProvinceDataByColNameAndYear")
 def get_province_data_by_colNameAndYear():
+    time.sleep(3)
     engine_str = "mysql+pymysql://{0}:{1}@{2}:{3}/{4}".format('root', '12345678', 'localhost', '3306', 'big_data_final')
     engine = create_engine(engine_str, encoding='utf-8')
     DBSession = sessionmaker(bind=engine)
     session = DBSession()
-    datas = session.execute("SELECT province.`name`, SUM(bigdata.city_income) AS 'value' FROM bigdata,city,province WHERE bigdata.`name` = city.`name` AND city.provincecode = province.`code` GROUP BY province.`name`")
+    colName = request.values.get('name')
+    year = request.values.get('year')
+    datas = session.execute("SELECT province.`name`, SUM(bigdata."+colName+") AS 'value' FROM bigdata,city,province WHERE bigdata.`name` = city.`name` AND city.provincecode = province.`code` AND bigdata.`year` = "+year+" GROUP BY province.`name`")
     resultList = []
     for i in datas.fetchall():
-        temp = bigVisualData(i.name, i.value)
+        temp = bigVisualData(str(i.name).replace('市', '').replace('省', '').replace(' ',''), i.value)
         resultList.append(temp)
     return ApiResponse.success(
         data=O2d.obj_to_list(resultList)
